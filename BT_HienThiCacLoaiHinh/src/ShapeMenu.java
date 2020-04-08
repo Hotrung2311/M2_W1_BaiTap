d1b8ba8e5a527d08535dbf6d0f7b0ae7aa7b1da7
Hotrung2311 <trungho2311@gmail.com>
2020-04-09 02:28:51 +0700

    Vẽ tam giác và tam giác cân

--- BT_HienThiCacLoaiHinh/src/ShapeMenu.java
+++ BT_HienThiCacLoaiHinh/src/ShapeMenu.java
@@ -14,7 +14,7 @@ public class ShapeMenu {
             choice = scanner.nextInt();
             switch (choice){
                 case 1:
-                    System.out.println("Draw the triangle");
+                    System.out.println("Print the rectangle");
                     System.out.print("Width: ");
                     int width = scanner.nextInt();
                     System.out.print("Height: ");
@@ -28,9 +28,67 @@ public class ShapeMenu {
                     }
                     break;
                 case 2:
+                    System.out.println("Print the square triangle");
+                    System.out.print("Width: ");
+                    width = scanner.nextInt();
+                    System.out.print("Height: ");
+                    height = scanner.nextInt();
 
+                    System.out.println("1. top-left");
+                    System.out.println("2. bottom-left");
+                    //System.out.println("3. top-right");
+                    //System.out.println("4. bottom-right");
+                    System.out.println("Enter your choice:");
+
+                    int choice1 = 1;
+                    choice1 = scanner.nextInt();
+
+                    switch (choice1){
+                        case 1:
+                            for (int i = 0; i < height; i++){
+                                System.out.println("");
+                                for (int j = i; j < width; j++){
+                                    System.out.print("* ");
+                                }
+                            }
+                            break;
+                        case 2:
+                            for (int i = 0; i < height; i++){
+                                System.out.println("");
+                                for (int j = 0; j < i+1 ; j++){
+                                    System.out.print("* ");
+                                }
+                            }
+                            break;
+//                        case 3:
+//                            for (int i = 0; i < height; i++){
+//                                System.out.println("");
+//                                for (int j = width-1; j >= i ; j--){
+//                                    System.out.print(" *");
+//                                }
+//                            }
+//                            break;
+                    }
                     break;
                 case 3:
+                    System.out.println("Print the isosceles triangle");
+                    System.out.print("Width: ");
+                    width = scanner.nextInt();
+                    System.out.print("Height: ");
+                    height = scanner.nextInt();
+                    for (int i = 0; i < height; i++){
+                        System.out.println("");
+                        for (int j = 0; j < i+1 ; j++){
+                            System.out.print("* ");
+                        }
+                    }
+                    for (int i = 1; i < height; i++){
+                        System.out.println("");
+                        for (int j = i; j < width; j++){
+                            System.out.print("* ");
+                        }
+                    }
+                    break;
                 case 4:
                     System.exit(0);
                     break;
